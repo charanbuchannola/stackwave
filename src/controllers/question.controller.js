f80d131bf9c863ea39c6689f26bd0d93e57c3ac2
charanbuchannola <buchannolacharan@gmail.com>
2025-04-19 23:37:14 +0530

    feat: enhance question creation validation and add endpoint to retrieve questions

--- src/controllers/question.controller.js
+++ src/controllers/question.controller.js
@@ -7,10 +7,9 @@ module.exports.createQuestionController = async (req, res) => {
     const { title, body, tags } = req.body;
     const userId = req.user._id;
 
-    if (!title || !body) {
-      return res.status(400).json({ error: "Title and body are required" });
+    if (!title || !body || !tags || tags.length === 0) {
+      return res.status(400).json({ error: "All fields are required." });
     }
-
     const newQuestion = await questionModel.create({
       title,
       body,
@@ -26,6 +25,20 @@ module.exports.createQuestionController = async (req, res) => {
     res.status(201).json({ newQuestion });
   } catch (err) {
     console.error("Error creating question:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports.getQuestionsController = async (req, res) => {
+  try {
+    const questions = await questionModel
+      .find({})
+      .populate("askedBy", "username media") // show user's basic info
+      .sort({ createdAt: -1 }); // latest questions first
+
+    res.status(200).json({ questions });
+  } catch (err) {
+    console.error("Error fetching questions:", err);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- src/routes/questionAnswer.routes.js
+++ src/routes/questionAnswer.routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const { authUser } = require("../middlewares/user.middleware");
 const {
   createQuestionController,
+  getQuestionsController,
 } = require("../controllers/question.controller");
 const router = express.Router();
 
 router.post("/createquestion", authUser, createQuestionController);
+
+router.get("/getquestions", authUser, getQuestionsController);
 
 module.exports = router;
